0090e7458c2a3a6f2a606b858dd1e729e7478155
Elise Harman <elise_harman@hotmail.com>
2021-04-11 15:39:34 +0800

    5 Day forecast dispalying using One Call API as per the spec

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var humidityValue = document.querySelector('.humidityValue')
 var windSpeed = document.querySelector('.windSpeed')
 var UvIndex = document.getElementById("UV-index")
 var fiveDayForecast = document.getElementById("weekForecast")
-
+var currentWeatherIcon = document.getElementById("weatherIcon")
 
 button.addEventListener('click', getCurrentDayForecastApi)
 
@@ -20,74 +20,98 @@ function getCurrentDayForecastApi(){
     .then(function (response){
         return response.json();
     })
-    .then(function (data) {        
+    .then(function (data) {   
+        console.log(data)     
         var nameValue = data.name;     
         var tempValue = data.main.temp;
+        console.log(data.main.temp)
         var humidValue = data.main.humidity;
         var windValue = data.wind.speed;
+        var weatherIcon = data.weather.icon;
 
         cityName.innerHTML = nameValue;
         temperature.innerHTML = tempValue +' ˚C ';
         humidityValue.innerHTML = humidValue +'˚%';
         windSpeed.innerHTML = windValue + ' MPH ';
-
-
-
+        //currentWeatherIcon.setAttribute(("src","https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png"));
         
         // //UV REQUEST
-        // //need a variable for latitude 
-        // //need a variable for latitude 
-        var APIKey = 'bee9bea7f570ee0519f49aaa8cf31eff';
-        var requestForCurrentUvData = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
-        console.log(requestForCurrentUvData)
+       var longitude = data.coord.lon;
+       console.log(data.coord.lon)
+       var latitude = data.coord.lat;
+       console.log(data.coord.lat)
+
+        var requestForExtendedForecastData = "https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&exclude=minutely,hourly&appid=bee9bea7f570ee0519f49aaa8cf31eff&units=metric";
+        console.log(requestForExtendedForecastData)
     
-        fetch(requestForCurrentUvData)
-        .then (function (response){
-            return response.json();
+        fetch(requestForExtendedForecastData)
+        .then (function (resp){
+            return resp.json();
         })
-        .then(function (data) {
-            console.log(data)
-            //not working 
-    })
+        .then(function (forecastData) {
+            console.log(forecastData.daily[0])
+            console.log(forecastData.daily[1])
+            console.log(forecastData.daily[2])
+            console.log(forecastData.daily[3])
+            console.log(forecastData.daily[4])
+
+    
+       
+    
+            for (let i = 1; i <= 6; i++) {
+                console.log(i)
+                
+                var fiveDayTemp = document.createElement('p')
+                var fiveDayHumidity = document.createElement('p')
+                var fiveDayWindSpeed = document.createElement('p')
+                fiveDayTemp.textContent = forecastData.daily[i].temp.day +' ˚C ';
+                fiveDayHumidity.textContent = forecastData.daily[i].humidity +'˚%';
+                fiveDayWindSpeed.textContent = forecastData.daily[i].wind_speed + ' MPH ';
+                fiveDayForecast.append(fiveDayTemp, fiveDayHumidity, fiveDayWindSpeed)
+    }
 })
-}
+})
+
+//One Call Function API 
+
+
 
 //Function to get API for the 5 day forecast 
 
-button.addEventListener('click', getFiveDayForecastApi)
+// button.addEventListener('click', getFiveDayForecastApi)
 
-function getFiveDayForecastApi(){
-    var requestUrlForFiveDayForecastApi = 'https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=bee9bea7f570ee0519f49aaa8cf31eff&units=metric'
-    console.log(requestUrlForFiveDayForecastApi)
+// function getFiveDayForecastApi(){
+//     var requestUrlForFiveDayForecastApi = 'https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&appid=bee9bea7f570ee0519f49aaa8cf31eff&units=metric'
+//     console.log(requestUrlForFiveDayForecastApi)
 
-    fetch(requestUrlForFiveDayForecastApi)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (fiveDayData) {
-        console.log(fiveDayData);
-        console.log(fiveDayData.list[0]);
-        console.log(fiveDayData.list[1]);
-        console.log(fiveDayData.list[2]);
-        console.log(fiveDayData.list[3]);
-        console.log(fiveDayData.list[4]);
+//     fetch(requestUrlForFiveDayForecastApi)
+//     .then(function (response){
+//         return response.json();
+//     })
+//     .then(function (fiveDayData) {
+//         console.log(fiveDayData);
+//         console.log(fiveDayData.list[0]);
+//         console.log(fiveDayData.list[1]);
+//         console.log(fiveDayData.list[2]);
+//         console.log(fiveDayData.list[3]);
+//         console.log(fiveDayData.list[4]);
 
    
 
-        for (let i = 0; i <= 5; i++) {
-            console.log(i)
+//         for (let i = 0; i <= 5; i++) {
+//             console.log(i)
             
-            var fiveDayTemp = document.createElement('p')
-            var fiveDayHumidity = document.createElement('p')
-            var fiveDayWindSpeed = document.createElement('p')
-            fiveDayTemp.textContent = fiveDayData.list[i].main.temp +' ˚C ';
-            fiveDayHumidity.textContent = fiveDayData.list[i].main.humidity +'˚%';
-            fiveDayWindSpeed.textContent = fiveDayData.list[i].wind.speed + ' MPH ';
-            fiveDayForecast.append(fiveDayTemp, fiveDayHumidity, fiveDayWindSpeed)
-        }
+//             var fiveDayTemp = document.createElement('p')
+//             var fiveDayHumidity = document.createElement('p')
+//             var fiveDayWindSpeed = document.createElement('p')
+//             fiveDayTemp.textContent = fiveDayData.list[i].main.temp +' ˚C ';
+//             fiveDayHumidity.textContent = fiveDayData.list[i].main.humidity +'˚%';
+//             fiveDayWindSpeed.textContent = fiveDayData.list[i].wind.speed + ' MPH ';
+//             fiveDayForecast.append(fiveDayTemp, fiveDayHumidity, fiveDayWindSpeed)
+//         }
         
         
-    })
+//     })
 
     // for (let i = 1; i <= 6; i++) {
     //     console.log(i)
@@ -98,7 +122,7 @@ function getFiveDayForecastApi(){
     //     fiveDayHumidity.textContent = 
     //     fiveDayWindSpeed.textContent = `Wind Speed: ${data.list[i].wind.speed}MPH`
 
-}
+// }
 
 
 // //SET CITY TO LOCAL STORAGE 
@@ -124,6 +148,6 @@ $("#day5").text(moment().add(5, 'days').format("dddd, MMM Do"));
 // //SET CITY TO LOCAL STORAGE
 //localStorage.setItem(inputValue, value);
 
-
+}
 
 
